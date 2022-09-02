1437144d41110efb3587f55088f18a8e080514b2
b97pla <pontus.larsson@medsci.uu.se>
2022-09-02 15:50:47 +0200

    include check that parameters are passed

--- create_nf_samplesheet.sh
+++ create_nf_samplesheet.sh
@@ -5,6 +5,21 @@ ANALYSISPATH="$2"
 DATAPATH="$3"
 ENVPATH="$4"
 
+if [[ $# -ne 4 ]]
+then
+  echo
+  echo "     This script requires the parameters <project name> <analysis path> <data path> <environment path>"
+  echo
+  echo "       example:"
+  echo "         $ create_nf_samplesheet.sh \\"
+  echo "             AB-1234 \\"
+  echo "             /proj/ngi2016001/nobackup/NGI/ANALYSIS \\"
+  echo "             /proj/ngi2016001/nobackup/NGI/DATA \\"
+  echo "             /vulpes/ngi/production/latest"
+  echo
+  exit 1
+fi
+
 PROJDIR="$ANALYSISPATH/$PROJ"
 DATADIR="$DATAPATH/$PROJ"
 
